2772c7e3c59a300471e57adcd416ac8273eb69c1
rialbat <miha-wead@mail.ru>
2022-10-30 18:45:42 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
+# IBM
 import ibmapi
 
+# GUI
+from PySide6 import QtWidgets, QtCore, QtGui
+import MainWindow
+
+programVersion = '0.1'
+
+
 def main():
-    print('Hi')
+    app = QtWidgets.QApplication(sys.argv)
+    window = ProgrammUI()
+    window.show()
+    app.exec()
 
 
 if __name__ == '__main__':
     main()
-
